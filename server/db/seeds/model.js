e86b2243a85ce1338eda71385afc615a5bed5404
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-02-13 22:35:03 +1300

    Delete year from model seeds

--- server/db/seeds/model.js
+++ server/db/seeds/model.js
@@ -6,9 +6,9 @@ exports.seed = function (knex) {
       .then(function () {
         // Inserts seed entries
         return knex("model").insert([
-          { id: 1, make_id: 1, name: "2006 Mitsubishi Outlander Vr-X"},
-          { id: 2, make_id: 2, name: "2015 Toyota Corolla Axio"},
-          { id: 3, make_id: 3, name: "1962 Holden Other EJ Premier"}
+          { id: 1, make_id: 1, name: "Mitsubishi Outlander Vr-X"},
+          { id: 2, make_id: 2, name: "Toyota Corolla Axio"},
+          { id: 3, make_id: 3, name: "Holden Other EJ Premier"}
         ]);
       });
   };
