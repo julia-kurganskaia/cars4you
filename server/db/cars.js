0b22656367bc1207042dfbc5ba18016a5ab4b1df
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-03-08 23:32:50 +1300

    Implement multiple joins

--- server/db/cars.js
+++ server/db/cars.js
@@ -1,7 +1,12 @@
+//@ts-check
+
 const connection = require('./connection')
 
 function getCars (db = connection) {
-  return db("car").select()
+  return db("car")
+  .join("location", "location.id", "car.location_id")
+  .join("model", "model.id", "car.model_id")
+  .select("*", "car.id AS id")
 }
 
 module.exports = {
