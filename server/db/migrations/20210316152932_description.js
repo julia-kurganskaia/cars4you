90afa4bb83bfa2a34d7775716f9862f8b786cbbd
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-03-17 22:38:33 +1300

    Change migration's name

--- server/db/migrations/20210316152932_description.js
+++ server/db/migrations/20210316152932_description.js
@@ -3,7 +3,6 @@
 exports.up = function(knex) {
   return knex.schema.createTable("description", table => {
       table.increments("id")
-      table.integer("car_id")
       table.string("about")
   })
 };
