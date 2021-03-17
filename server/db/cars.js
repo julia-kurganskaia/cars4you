2f8cb8c0b6c9dd98e15a0df808daf050049a8ca1
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-03-17 22:52:14 +1300

    Read car's description from description table

--- server/db/cars.js
+++ server/db/cars.js
@@ -6,6 +6,7 @@ function getCars (db = connection) {
   return db("car")
   .join("location", "location.id", "car.location_id")
   .join("model", "model.id", "car.model_id")
+  .join("description", "description.id", "car.description_id")
   .select("*", "car.id AS id")
 }
 
