8cc754148b0c059a728ced2694b43a8e70d532a4
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-03-17 22:45:30 +1300

    Delete curly braces around link parameter

--- client/components/ContactUs.jsx
+++ client/components/ContactUs.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function ContactUs() {
     return (
         <div>
-            <Link to={"/"}>Home</Link>
+            <Link to="/">Home</Link>
             <div>Would you like to find out more and want to talk to someone? Contact us by clicking on one of the boxes below to let us know if you have any questions.
                 <div>
                     <div>Call us</div>
