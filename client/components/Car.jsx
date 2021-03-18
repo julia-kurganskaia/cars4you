f38f05e77a3ddee4100f9cad2093c17543095e92
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-03-18 16:33:00 +1300

    Display one car on the page

---
+++ client/components/Car.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+// import { fetchCars } from "../actions";
+import { getCarById } from "../apis/cars";
+// import { getCarById } from "../apis/cars";
+
+function Car (props) {
+    // useEffect(() => {
+    //   props.dispatch(fetchCars())
+    //   }, []);
+    const carId = props.match.params.id;
+    const data = props.cars;
+
+    console.log(carId, data)
+
+    for (let i = 0; i <= data.length; i++) {
+        let displayingCar;
+        if (data[i].id == carId) {
+            displayingCar = data[i];
+            console.log(displayingCar.id)
+            return (
+                <div className="oneCar">
+                    <div>
+                        <img src={"listings-images/" + displayingCar.id.toString() + ".jpg"} />
+                    </div>
+                    <div>
+                        <div className="cars-detail">
+                            {displayingCar.location}
+                        </div>
+                        <div className="cars-detail">
+                            {displayingCar.name}
+                        </div>
+                        <div className="cars-detail">
+                            {displayingCar.year}
+                        </div>
+                        <div className="cars-detail">
+                            Colour: {displayingCar.colour}
+                        </div>
+                        <div className="cars-detail">
+                            Seats: {displayingCar.seats}
+                        </div>
+                        <div className="cars-detail">
+                            Odometer: {displayingCar.odometer}
+                        </div>
+                        <div className="cars-detail">
+                            <div>Engine details</div>
+                            <div>
+                                {displayingCar.engine}, {displayingCar.fuel}, {displayingCar.transmission}
+                            </div>
+                        </div>
+                        <div className="cars-detail">
+                            {displayingCar.about}
+                        </div>
+                        <div className="cars-detail">
+                            Asking price: $ {displayingCar.price}
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+}
+
+const mapStateToProps = (globalState) => {
+    return {
+      cars: globalState.cars
+    }
+ };
+
+
+export default connect(mapStateToProps)(Car)
