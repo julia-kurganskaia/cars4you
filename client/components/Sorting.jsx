ef2a02a4f0a53d86c3f9a002829b4410bbb9f647
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-03-22 16:07:32 +1300

    Implement functions to handle click to toggle price preferences

--- client/components/Sorting.jsx
+++ client/components/Sorting.jsx
@@ -1,19 +1,44 @@
-import React from "react";
-
 //@ts-check
 
-function Sorting () {
+import React from "react";
+import { connect } from "react-redux";
+import { togglePrice } from "../actions";
+
+function Sorting (props) {
+
+    const lowestFirstByClick = () => {
+        if (props.sorting.pricelowestFirst === false) {
+            props.dispatch(togglePrice());
+        }
+    };
+
+    const highestFirstByClick = () => {
+        if (props.sorting.pricelowestFirst === true) {
+            props.dispatch(togglePrice());
+        }
+    };
+
+    console.log(props.sorting, 'bbb')
+
     return (
         <div className="sorting">
-            <div>Sort by:</div>
-            <div className="sorting">
-                <div>Price</div>
-                <i className="fas fa-angle-down"></i>
-                <i className="fas fa-angle-up"></i>
+            {JSON.stringify(props.sorting)}
+            <div id="sort-by">Sort by:</div>
+            <div className="sorting-by-price">
+                <div id="price">Price</div>
+                <i onClick={lowestFirstByClick} className="fas fa-angle-down angle"></i>
+                <i onClick={highestFirstByClick} className="fas fa-angle-up angle"></i>
             </div>
         </div>
     )
 }
 
-export default Sorting
+const mapStateToProps = (globalState) => {
+    return {
+      cars: globalState.cars,
+      sorting: globalState.sorting,
+    }
+  };
 
+
+  export default connect(mapStateToProps)(Sorting)
