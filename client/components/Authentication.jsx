7d9458385b846e99a01554ae3f63a5c2e788a84b
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-07 16:00:55 +1200

    Add login form

--- client/components/Authentication.jsx
+++ client/components/Authentication.jsx
@@ -6,8 +6,20 @@ import { Link } from "react-router-dom";
 function Authentication() {
     return (
         <div>
-            <Link to="/">Home</Link>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
+            <div className="login-form">
+                <h1>Login</h1>
+                <form>
+                    <input className="login-input" placeholder="Email" type="text"></input>
+                    <input className="login-input" placeholder="Password" type="text"></input>
+                    <button className="login-submit">Submit</button>
+                </form>
+            </div>
+
         </div>
+
     )
 };
 
