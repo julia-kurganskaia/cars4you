7a69688e17223ab5544004051edd90a0a250010c
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-09 17:36:18 +1200

    Change routes name

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const carRoutes = require("./routes/cars")
-const usersRoutes = require("./routes/users")
+const authRoutes = require("./routes/users")
 
 const server = express()
 
@@ -10,6 +10,6 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use("/api/v1/cars", carRoutes)
-server.use("/api/v1/auth", usersRoutes)
+server.use("/api/v1/auth", authRoutes)
 
 module.exports = server
