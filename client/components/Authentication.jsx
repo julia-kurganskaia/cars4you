4b3161c0da4078805a37b870fc261af96b9f27e6
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-10 19:34:56 +1200

    Get value from input field

--- client/components/Authentication.jsx
+++ client/components/Authentication.jsx
@@ -1,24 +1,40 @@
 //@ts-check
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Authentication() {
-       return (
-        <div>
-            <div>
-                <Link to="/">Home</Link>
-            </div>
-            <div className="login-form">
-                <h1>Login</h1>
-                <form>
-                    <input className="login-input" placeholder="Email" type="text"></input>
-                    <input className="login-input" placeholder="Password" type="text"></input>
-                    <button className="login-submit">Submit</button>
-                </form>
-            </div>
+  const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+  })
+
+  function handleChange(e) {
+    e.persist();
+
+    setFormData(currentFormData => {
+      return ({
+        ...currentFormData,
+        [e.target.name]: e.target.value,
+      });
+    });
+  }
+console.log(formData)
+  return (
+    <div>
+      <div>
+        <Link to="/">Home</Link>
         </div>
-    )
+          <div className="login-form">
+            <h1>Login</h1>
+              <form>
+                <input className="login-input" placeholder="Email" type="text" name="email" value={formData.email} onChange={handleChange}></input>
+                <input className="login-input" placeholder="Password" type="text" name="password" value={formData.password} onChange={handleChange}></input>
+                <button className="login-submit">Submit</button>
+              </form>
+          </div>
+      </div>
+  )
 };
 
 export default Authentication
