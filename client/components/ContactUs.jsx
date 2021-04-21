e189ba2b616bf2e4e6c8f5096a219f5fd0205646
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-22 10:35:53 +1200

    Style contact us page

--- client/components/ContactUs.jsx
+++ client/components/ContactUs.jsx
@@ -4,12 +4,26 @@ import { Link } from "react-router-dom";
 function ContactUs() {
     return (
         <div>
-            <Link to="/">Home</Link>
-            <div>Would you like to find out more and want to talk to someone? Contact us by clicking on one of the boxes below to let us know if you have any questions.
-                <div>
-                    <div>Call us</div>
-                    <div>Email us</div>
-                    <div>Visit us</div>
+            <div className="small-header">
+                <div className="home-container">
+                    <Link className="home-link" to="/">Home</Link>
+                </div>
+            </div>
+            <div className="contact-us-container">
+                <div className="content">
+                    <div className="contact-us-text">
+                        <div className="text-container">
+                            Would you like to find out more and want to talk to someone?
+                        </div>
+                        <div className="text-container">
+                            Contact us by clicking on one of the boxes below to let us know if you have any questions.
+                        </div>
+                    </div>
+                    <div id="contact-information">
+                        <div className="contact">Call us:  +64 22 123 4567</div>
+                        <div className="contact">Email us:  cars4you@cars.com</div>
+                        <div className="contact">Visit us:  1 Fifth Avenue, Wellington, New Zealand</div>
+                    </div>
                 </div>
             </div>
         </div>
