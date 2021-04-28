1c92df424fad0a7f8c18b795ba5c0a8cebf49725
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-28 22:15:21 +1200

    Add session file store for Express

--- server/server.js
+++ server/server.js
@@ -3,16 +3,21 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
+const FileStore = require("session-file-store")(session);
 
 const carRoutes = require("./routes/cars");
 const authRoutes = require("./routes/users");
+const profileRoutes = require("./routes/profile");
 
 const server = express();
 
 server.use(express.json());
 server.use(express.static(path.join(__dirname, 'public')));
 
+const fileStoreOptions = {};
+
 server.use(session({
+    store: new FileStore(fileStoreOptions),
     secret: "kioiuj98uy86gtp5rtg",
     resave: true,
     saveUninitialized: false,
@@ -20,5 +25,6 @@ server.use(session({
 
 server.use("/api/v1/cars", carRoutes);
 server.use("/api/v1/auth", authRoutes);
+server.use("/api/v1/profile", profileRoutes);
 
 module.exports = server
