7215213c66231252ceff6ee17837b0c01e5dddf8
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-05-20 23:48:17 +1200

    Save user's name in sessionStorage

--- client/actions/index.js
+++ client/actions/index.js
@@ -33,6 +33,12 @@ export function signUpUser(name, email, password) {
   return (dispatch) => {
     return signUp(name, email, password)
       .then(user => {
+        sessionStorage.setItem("userName", user.body.user_name);
+        history.push("/");
+        console.log(user.body.user_name, user.body)
+        return user.body;
+      })
+      .then(user => {
         dispatch(registerUser(user))
       })
       .catch(() => {
